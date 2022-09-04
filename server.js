14a2b39ac085997eb24ee41737d6127aa31a4dbd
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2022-09-04 13:11:14 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+ 'use strict';
+
+ require('dotenv').config({silent: true});
+ 
+ var server = require('./app');
+ var port = process.env.PORT || 3000;
+ 
+ server.listen(port, function() {
+   // eslint-disable-next-line
+   console.log('Server running on port: %d', port);
+ });
+ 
